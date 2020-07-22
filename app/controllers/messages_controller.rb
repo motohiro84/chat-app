9461cf499b3102d410182d949cfa238f9b1f7573
motohiro84 <motohiro84@users.noreply.github.com>
2020-07-22 11:16:58 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -12,6 +13,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       render :index
+    end
   end
 
   private
